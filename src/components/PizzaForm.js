60331ce780b7d2256fcf68c255919de29590e16b
richardbobo1 <richard.bobo@gmail.com>
2020-05-19 17:29:50 -0400

    Done.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,16 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
   return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
+            <input onChange={props.handleChange} type="text" name='topping' className="form-control" placeholder="Pizza Topping" value={
                 //Pizza Topping Should Go Here
-                null
+                props.pizza.topping
               }/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select onChange={props.handleChange} value={props.pizza.size} name='size' className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,20 +18,20 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input onChange={props.onVegeChange}  className="form-check-input" type="radio" value="Vegetarian" checked={props.pizza.vegetarian}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input onChange={props.onVegeChange} className="form-check-input" type="radio" value="Not Vegetarian" checked={!props.pizza.vegetarian}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={props.onFormSubmit}>Submit</button>
         </div>
       </div>
 
